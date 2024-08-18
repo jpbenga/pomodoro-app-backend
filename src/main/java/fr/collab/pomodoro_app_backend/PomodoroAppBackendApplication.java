bfe432eee3658607c07fbef5a2d4350a8afb3e0d
jpbenga <jpaulbenga@gmail.com>
2024-08-18 15:20:07 +0200

    Initial commit

---
+++ src/main/java/fr/collab/pomodoro_app_backend/PomodoroAppBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.collab.pomodoro_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/collab/pomodoro_app_backend/PomodoroAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.collab.pomodoro_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
